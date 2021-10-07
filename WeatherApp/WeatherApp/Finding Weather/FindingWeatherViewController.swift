a26d9e9b7b4c625372bf81b494a1b5e6b2d4b6d0
Alaa Taher <aabuelmagd@banquemisr.com>
2021-10-07 13:55:39 +0200

    Story #2

--- WeatherApp/WeatherApp/Finding Weather/FindingWeatherViewController.swift
+++ WeatherApp/WeatherApp/Finding Weather/FindingWeatherViewController.swift
@@ -6,17 +6,28 @@
 //
 
 import UIKit
+import SVProgressHUD
+import SwiftMessageBar
 
 class FindingWeatherViewController: UIViewController {
 
     @IBOutlet weak var searchTextField: UITextField!
+    private let viewModel = FindingWeatherViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+        setupBinding()
     }
 
     private func setupView() {
         searchTextField.delegate = self
+    }
+
+    @IBAction func goButtonAction(_ sender: Any) {
+        guard let text = searchTextField.text else {
+            return
+        }
+        viewModel.loadWeatherInfo(with: text)
     }
 }
 
@@ -24,5 +35,25 @@ extension FindingWeatherViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+}
+
+//Binding
+extension FindingWeatherViewController {
+    private func setupBinding() {
+        setupLoadingBinding()
+        setupErrorBinding()
+    }
+
+    private func setupErrorBinding() {
+        viewModel.errorMessage = { message in
+            SwiftMessageBar.showMessage(withTitle: "Error", message: message , type: .error)
+        }
+    }
+
+    private func setupLoadingBinding() {
+        viewModel.isLoading = { isload in
+            isload ? SVProgressHUD.show() : SVProgressHUD.dismiss()
+        }
     }
 }
